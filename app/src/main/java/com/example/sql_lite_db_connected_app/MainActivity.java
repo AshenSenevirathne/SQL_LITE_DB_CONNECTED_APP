3905419548a05588bc08c3a50487ab93779b8daf
AshenSenevirathne <ashensachin@gmail.com>
2019-10-02 08:49:05 +0530

    Sql Lite db connected app created

---
+++ app/src/main/java/com/example/sql_lite_db_connected_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sql_lite_db_connected_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
